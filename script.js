b6972febb31f304a248e7a34d54d2b1330fa790e
victorveror <akunkedua2299@gmail.com>
2024-08-12 14:51:26 +0700

    perubahan

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkAnswers() {
     const q1 = parseInt(document.getElementById('q1').value);
     const q2 = parseInt(document.getElementById('q2').value);
@@ -28,34 +27,3 @@ function checkAnswers() {
     const result = document.getElementById('result');
     result.textContent = `Skor Anda: ${score} dari 10`;
 }
-=======
-function checkAnswers() {
-    const q1 = parseInt(document.getElementById('q1').value);
-    const q2 = parseInt(document.getElementById('q2').value);
-    const q3 = parseInt(document.getElementById('q3').value);
-    const q4 = parseInt(document.getElementById('q4').value);
-    const q5 = parseInt(document.getElementById('q5').value);
-    const q6 = parseInt(document.getElementById('q6').value);
-    const q7 = parseInt(document.getElementById('q7').value);
-    const q8 = parseInt(document.getElementById('q8').value);
-    const q9 = parseInt(document.getElementById('q9').value);
-    const q10 = parseInt(document.getElementById('q10').value);
-
-
-    let score = 0;
-
-    if (q1 === 1) score++;
-    if (q2 === 2) score++;
-    if (q3 === 3) score++;
-    if (q4 === 4) score++;
-    if (q5 === 5) score++;
-    if (q6 === 6) score++;
-    if (q7 === 7) score++;
-    if (q8 === 8) score++;
-    if (q9 === 9) score++;
-    if (q10 === 10) score++;
-
-    const result = document.getElementById('result');
-    result.textContent = `Skor Anda: ${score} dari 10`;
-}
->>>>>>> a622e6830934a661e14aecb4e9da723976adb8e5
--- script9.js
+++ script9.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function checkAnswers() {
     const q1 = parseInt(document.getElementById('q1').value);
     const q2 = parseInt(document.getElementById('q2').value);
@@ -27,34 +26,4 @@ function checkAnswers() {
 
     const result = document.getElementById('result');
     result.textContent = `Skor Anda: ${score} dari 10`;
-=======
-function checkAnswers() {
-    const q1 = parseInt(document.getElementById('q1').value);
-    const q2 = parseInt(document.getElementById('q2').value);
-    const q3 = parseInt(document.getElementById('q3').value);
-    const q4 = parseInt(document.getElementById('q4').value);
-    const q5 = parseInt(document.getElementById('q5').value);
-    const q6 = parseInt(document.getElementById('q6').value);
-    const q7 = parseInt(document.getElementById('q7').value);
-    const q8 = parseInt(document.getElementById('q8').value);
-    const q9 = parseInt(document.getElementById('q9').value);
-    const q10 = parseInt(document.getElementById('q10').value);
-
-
-    let score = 0;
-
-    if (q1 === 9) score++;
-    if (q2 === 18) score++;
-    if (q3 === 27) score++;
-    if (q4 === 36) score++;
-    if (q5 === 45) score++;
-    if (q6 ===54) score++;
-    if (q7 === 63) score++;
-    if (q8 === 72) score++;
-    if (q9 === 81) score++;
-    if (q10 === 90) score++;
-
-    const result = document.getElementById('result');
-    result.textContent = `Skor Anda: ${score} dari 10`;
->>>>>>> a622e6830934a661e14aecb4e9da723976adb8e5
 }
